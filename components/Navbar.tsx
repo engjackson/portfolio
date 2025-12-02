f24037a47293461d90b4cc24f8269fe24c57ef1b
Jackson Eng <jacksoneng@Jacksons-Mac-mini.local>
2025-12-02 12:09:29 -0500

    updated resume

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -54,7 +54,7 @@ export function Navbar() {
             </a>
           ))}
           <a
-            href="/EngJacksonProdMgrResume.pdf"
+            href="/EngJacksonProdMgrResume.pdf target=_blank"
             className="rounded-full bg-zinc-900 px-4 py-1.5 text-xs font-semibold text-white shadow-sm hover:bg-zinc-800 transition-colors"
           >
             Download Resume
